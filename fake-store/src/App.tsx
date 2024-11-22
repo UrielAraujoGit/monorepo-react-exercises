f138ca6535d40f482bef45b68acbe59499aef190
uriel <urielaraujo@live.com.ar>
2024-11-22 20:17:05 -0300

    fake-store/ABC-123 crateProduct list & getProductList

--- fake-store/src/App.tsx
+++ fake-store/src/App.tsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { TProduct } from "./models/product.type";
 
 function App() {
+  const [products, setProducts] = useState<Array<TProduct>>([]);
+
+  const getProducts = async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <>
       <h1 className="text-center text-3xl">fake store</h1>
+      {products.map((prod) => (
+        <div key={prod.id}>
+          <p>{prod.title}</p>
+          <img src={prod.image} alt={prod.title} />
+          <p>{prod.description}</p>
+          <p>{prod.price}</p>
+        </div>
+      ))}
     </>
   );
 }
---
+++ fake-store/src/models/product.type.ts
@@ -0,0 +1,14 @@
+export type TProduct = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: TRating;
+};
+
+export type TRating = {
+  rate: number;
+  count: number;
+};
