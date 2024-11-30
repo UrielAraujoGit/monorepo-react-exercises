8e598e51fc198d28a0f8b2f2713ae8c27da5b22c
SuspiciousSchrodinger <kenicorto1@hotmail.com>
2024-11-29 21:22:18 -0300

    greetings-bot/868ajgy4d/EFSR se añade funcionalidad on click, tarea completa

--- greetings-bot/src/App.tsx
+++ greetings-bot/src/App.tsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <>
       <h1 className="text-center text-3xl">greetings bot</h1>
+      <button className="border bg-slate-600
+      " onClick={(e) => {console.log("Hola!")}}> Saludo </button>
     </>
   );
 }
