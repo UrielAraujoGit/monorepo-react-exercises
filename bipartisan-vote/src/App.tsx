67c700254a52886670870893d47bc7759da688e8
SuspiciousSchrodinger <kenicorto1@hotmail.com>
2024-11-29 21:47:14 -0300

    bipartisan-vote/868ajh3c7/EFSR

--- bipartisan-vote/src/App.tsx
+++ bipartisan-vote/src/App.tsx
@@ -1,9 +1,28 @@
+import { useCallback, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [votobatman, setvotobatman] = useState(0);
+  const [votosuperman, setvotosuperman] = useState(0);
   return (
     <>
       <h1 className="text-center text-3xl">bipartisan vote</h1>
+      <div
+        onClick={(e) => {
+          setvotosuperman(votosuperman + 1);
+        }}
+      >
+        Superman
+      </div>
+      <div>{votosuperman}</div>
+      <div
+        onClick={(e) => {
+          setvotobatman(votobatman + 1);
+        }}
+      >
+        batman
+      </div>
+      <div>{votobatman}</div>
     </>
   );
 }
