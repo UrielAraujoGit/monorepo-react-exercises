0d426b556747f6239c6d91c195a94d208d84da4a
SuspiciousSchrodinger <kenicorto1@hotmail.com>
2024-11-29 21:00:28 -0300

    click-machine/868ajh1j2 Se agrega contador, tarea completa

--- click-machine/src/App.tsx
+++ click-machine/src/App.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [cantidad, setcantidad] = useState(0);
   return (
-    <>
+    <div>
       <h1 className="text-center text-3xl">click machine</h1>
-    </>
+      <h1 className="allign justify-self-cente">{cantidad}</h1>
+      <button
+        onClick={(e) => {
+          const cantidadnueva = cantidad + 1;
+          console.log(cantidad);
+          setcantidad(cantidadnueva)
+        }}
+      >
+        botton
+      </button>
+    </div>
   );
 }
 
