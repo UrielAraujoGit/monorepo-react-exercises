0f1899d9fc9f7ef0d3be4019f745df16ad752ee3
amarrero86mr <amarrero86mr@gmail.com>
2024-12-23 19:20:51 -0300

    task-management/868ax78hp adding style to the new task modal - tasks.style.css

--- task-management/src/components/tasks&subtasks/newtask.modal.tsx
+++ task-management/src/components/tasks&subtasks/newtask.modal.tsx
@@ -1,20 +1,24 @@
 import { useState } from "react";
 import { TSubTask } from "../../types/board.type";
 
-export const TaskModal = (props: { 
-    fnOnOffBtnTaskModal:()=>void,
-    fnNewId:()=>void,
-    idToDo:number,
-    fnNewTasks:(addNameTask:string, addSubTasks:Array<TSubTask>)=>void
-    
+export const TaskModal = (props: {
+    fnOnOffBtnTaskModal: () => void,
+    fnNewId: () => void,
+    idToDo: number,
+    fnNewTasks: (addNameTask: string, addSubTasks: Array<TSubTask>) => void
+
 }) => {
-    
+
     const [addNameTask, setAddNameTask] = useState<string>('')
     const [addSubTasks, setAddSubTasks] = useState<Array<TSubTask>>([])
-    const [ addNames, setAddNames ] = useState('')
-    
-    
-    const moreSubTask = () =>{
+    const [addNames, setAddNames] = useState('')
+
+
+    const moreSubTask = () => {
+        if (addNames.trim() === ''){
+            alert("la sub tarea ya existe")
+            return
+        }
         const existName = addSubTasks.some(item => item.name === addNames)
         if (existName) {
             alert("la sub tarea ya existe")
@@ -30,51 +34,54 @@ export const TaskModal = (props: {
 
     }
 
-    
+
     return (
         <>
-            <div 
-            className="flex flex-col w-52 "
-            >
-                <input
-                    className="border border-sky-800 text-gray-900 rounded-md m-1"
-                    type="text"
-                    placeholder="Task Name"
-                    value={addNameTask}
-                    onChange={(e) => setAddNameTask(e.target.value)}
-                />
-                <ul>
-                    {addSubTasks.map(item => {
-                        return (
-                            <li 
-                            key={item.id}
-                            >{item.name}</li>
-                        )
-                    })}
-                </ul>
-                <input 
-                    className="border border-sky-800 text-gray-900 rounded-md m-1"
-                    type="text"
-                    placeholder="SUB-Task Name"
-                    value={addNames}
-                    onChange={(e) => setAddNames(e.target.value)} />
-                <button
-                    className="border border-green-800"
-                    onClick={() => {
-                        moreSubTask();
-                        setAddNames('')
-                    }}
-                >+ Sub Task</button>
-                <button
-                    className="border border-green-800"
-                    onClick={() => {props.fnNewTasks(addNameTask, addSubTasks)
-                        props.fnOnOffBtnTaskModal()
-                    }}
-                >ADD Task</button>
-                <button
-                    className="border border-green-800"
-                    onClick={() => { props.fnOnOffBtnTaskModal() }}
-                >Cancel</button>
+            <div className="new-task-modal-container">
+                <div
+                    className="flex flex-col new-task-form"
+                >
+                    <input
+                        className="border border-sky-800 text-gray-900 rounded-md m-1"
+                        type="text"
+                        placeholder="Task Name"
+                        value={addNameTask}
+                        onChange={(e) => setAddNameTask(e.target.value)}
+                    />
+                    <ul>
+                        {addSubTasks.map(item => {
+                            return (
+                                <li
+                                    key={item.id}
+                                >{item.name}</li>
+                            )
+                        })}
+                    </ul>
+                    <input
+                        className="border border-sky-800 text-gray-900 rounded-md m-1"
+                        type="text"
+                        placeholder="SUB-Task Name"
+                        value={addNames}
+                        onChange={(e) => setAddNames(e.target.value)} />
+                    <button
+                        className="border border-green-800"
+                        onClick={() => {
+                            moreSubTask();
+                            setAddNames('')
+                        }}
+                    >+ Sub Task</button>
+                    <button
+                        className="border border-green-800"
+                        onClick={() => {
+                            props.fnNewTasks(addNameTask, addSubTasks)
+                            props.fnOnOffBtnTaskModal()
+                        }}
+                    >ADD Task</button>
+                    <button
+                        className="border border-green-800"
+                        onClick={() => { props.fnOnOffBtnTaskModal() }}
+                    >Cancel</button>
+                </div>
             </div>
         </>
     )
--- task-management/src/components/tasks&subtasks/tasks.tsx
+++ task-management/src/components/tasks&subtasks/tasks.tsx
@@ -2,25 +2,25 @@ import { TTask } from "../../types/board.type";
 import { SubTasks } from "./subtasks";
 import "./tasks.style.css"
 
-export const Tasks = (props: {dataTask: Array<TTask>}) => {
+export const Tasks = (props: { dataTask: Array<TTask> }) => {
 
     return (
         <>
-            <div className="task-container">
-                {props.dataTask.map(itemtask => {
-                    return(
-                        <div key={itemtask.id}
+
+            {props.dataTask.map(itemtask => {
+                return (
+                    <div key={itemtask.id}
                         className="task-card cursor-pointer"
-                        onClick={()=>{}}
-                        >
-                            <h3>{itemtask.name}</h3>
-                            <p>{itemtask.subtasks.length} substasks</p>
-                            <SubTasks
+                        onClick={() => { }}
+                    >
+                        <h3>{itemtask.name}</h3>
+                        <p>{itemtask.subtasks.length} substasks</p>
+                        <SubTasks
                             dataSubTasks={itemtask.subtasks}></SubTasks>
-                        </div>                       
-                    )
-                })}
-            </div>
+                    </div>
+                )
+            })}
+
         </>
     )
 }
