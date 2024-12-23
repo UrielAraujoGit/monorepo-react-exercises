8aa0d1fbb6aad6380ec65ff64761d82caf8c405a
amarrero86mr <amarrero86mr@gmail.com>
2024-12-23 15:50:59 -0300

    task-management/868ax78hp adding position style on new task button

--- task-management/src/components/states/states.board.tsx
+++ task-management/src/components/states/states.board.tsx
@@ -3,6 +3,7 @@ import { TState, TSubTask } from "../../types/board.type";
 import { Tasks } from "../tasks&subtasks/tasks";
 import { StateModal } from "./newstate.modal";
 import { TaskModal } from "../tasks&subtasks/newtask.modal";
+import "./state.style.css";
 
 
 export const StateBoard = (props: 
@@ -26,6 +27,10 @@ export const StateBoard = (props:
     
     return (
         <>
+            <button
+                    className="border border-emerald-800 bg-emerald-500 text-gray-700 m-1 btn-new-task"
+                    onClick={()=>{onOffBtnTaskModal()}}
+                    >➕ task</button>
             <div className="state-container">
             <button 
             className="border border-green-800 bg-green-500 text-gray-700 m-1"
@@ -50,10 +55,7 @@ export const StateBoard = (props:
                         </div>
                     )
                 })}
-                <button
-                    className="border border-emerald-800 bg-emerald-500 text-gray-700 m-1"
-                    onClick={()=>{onOffBtnTaskModal()}}
-                    >➕ task</button>
+                
             </div>
             {btnNewTaskModal? 
             <TaskModal
