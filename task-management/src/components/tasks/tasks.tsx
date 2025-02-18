63f2c0246d004ed61a12476559c346ad5a903442
amarrero86mr <amarrero86mr@gmail.com>
2025-02-18 19:31:01 -0300

    task-management/868ax78hp refactor tasks list

--- task-management/src/components/tasks/tasks.tsx
+++ task-management/src/components/tasks/tasks.tsx
@@ -1,15 +1,10 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { BoardsContext } from "../board-context/boards.context";
 import { SubTasksComponent } from "../subtasks/subtasks";
 
 export const TaskComponent = (props: { stateId: number }) => {
   const { boardSelected, boards } = useContext(BoardsContext);
-
-  //   const [tasksShow, setTasksShow] = useState<TTask[] | null>();
-
-  //   useEffect(() => {
-  //     setTasksShow(props.tasksForState);
-  //   }, [boardSelected]);
+  
   const board = boards.find((b) => b.id === boardSelected);
   const state = board?.states.find((s) => s.id === props.stateId);
   const tasksShow = state?.tasks;
@@ -19,9 +14,18 @@ export const TaskComponent = (props: { stateId: number }) => {
     <>
       <ul>
         {tasksShow?.map((item) => (
-          <li className="m-2 p-3 bg-slate-800 rounded-md" key={item.id}>
-            <h4 className="mt-3 m-1">{item.name}</h4>
-            <SubTasksComponent subtasks={item.subtasks}></SubTasksComponent>
+          <li
+            
+            className="m-2 p-3 bg-slate-800 rounded-md" key={item.id}>
+            <h4 className="m-1 cursor-pointer">{item.name}</h4>
+            {/* <SubTasksComponent subtasks={item.subtasks}></SubTasksComponent> */}
+
+            <p>
+              {(Object.values(item.subtasks).filter(k => k.completed === true).length)}
+              {' '} of {' '}
+              {item.subtasks.length} sub-tasks
+            </p>
+            
           </li>
         ))}
       </ul>
